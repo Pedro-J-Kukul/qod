693347ce9f253d666e8ac301dbe8a31a053d4823
Pedro-J-Kukul <pedro.j.kukul@gmail.com>
2025-08-27 02:29:34 -0600

    Added CUstom errors

---
+++ cmd/api/errors.go
@@ -0,0 +1,57 @@
+// Filename: cmd/api/errors.go
+// Description: Handling errors in proper json format
+
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+// error helper function to correctly log an error
+func (app *application) logError(r *http.Request, err error) {
+	method := r.Method
+	uri := r.URL.RequestURI()
+	app.logger.Error(err.Error(), "method", method, "uri", uri)
+}
+
+// Sends an error response in JSON format
+func (app *application) errorResponseJSON(w http.ResponseWriter, r *http.Request, status int, message any) {
+	// create an envelope of error data
+	errorData := envelope{"error": message}
+	err := app.writeJSON(w, status, errorData, nil)
+	if err != nil {
+		// log the error
+		app.logError(r, err)
+		w.WriteHeader(500)
+	}
+}
+
+// sends an error in case our server is kabloey
+func (app *application) serverErrorResponse(w http.ResponseWriter, r *http.Request, err error) {
+	// log the error
+	app.logError(r, err)
+	// prepare a message to send to the clietn
+	message := "the server encountered a problem and could not process your request"
+	app.errorResponseJSON(w, r, http.StatusInternalServerError, message)
+}
+
+// send an error response if our client messes up with a 404
+func (app *application) notFoundResponse(w http.ResponseWriter,
+	r *http.Request) {
+
+	// we only log server errors, not client errors
+	// prepare a response to send to the client
+	message := "the requested resource could not be found"
+	app.errorResponseJSON(w, r, http.StatusNotFound, message)
+}
+
+// send an error response if our client messes up with a 405
+func (app *application) methodNotAllowedResponse(w http.ResponseWriter, r *http.Request) {
+
+	// we only log server errors, not client errors
+	// prepare a formatted response to send to the client
+	message := fmt.Sprintf("the %s method is not supported for this resource", r.Method)
+
+	app.errorResponseJSON(w, r, http.StatusMethodNotAllowed, message)
+}
--- cmd/api/handlers.go
+++ cmd/api/handlers.go
@@ -23,8 +23,7 @@ func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Reques
 	// call helper function to write to json
 	err := app.writeJSON(w, http.StatusOK, data, nil)
 	if err != nil {
-		app.logger.Error(err.Error())
-		http.Error(w, "The Server encountered a problem and could not process your request", http.StatusInternalServerError)
+		app.serverErrorResponse(w, r, err)
 	}
 
 }
